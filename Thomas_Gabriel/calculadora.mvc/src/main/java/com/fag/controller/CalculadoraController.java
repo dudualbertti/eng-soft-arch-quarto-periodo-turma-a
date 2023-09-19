0b5ae19345086d2a60b2c996cf12c5bd36902015
thomasgsales <tgaabriel444@gmail.com>
2023-09-19 20:39:11 -0300

    trabalho

--- Thomas_Gabriel/calculadora.mvc/src/main/java/com/fag/controller/CalculadoraController.java
+++ Thomas_Gabriel/calculadora.mvc/src/main/java/com/fag/controller/CalculadoraController.java
@@ -1,8 +1,9 @@
 package com.fag.controller;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
@@ -20,9 +21,46 @@ public class CalculadoraController {
     }
 
     @GET
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get(@QueryParam("name") String name) {
-        return page.data("name", name);
+    @Produces (MediaType.TEXT_HTML)
+    public TemplateInstance get(){
+        return page.data("result", 0d);
     }
+
+    @POST
+    @Produces(MediaType.TEXT_HTML) 
+    public TemplateInstance get(
+        @FormParam("numero1") Double num1,
+        @FormParam("numero2") Double num2,
+        @FormParam("operation") String operation) {
+        
+            Double resultado = (double) 0;
+
+            switch(operation) {
+                case "Soma":
+                    resultado = num1 + num2;
+                    break;
+                case "Divisão":
+                    if(num2 != 0){
+                        resultado = num1 / num2;
+                    }
+                    else {
+                        resultado = Double.NaN; 
+                    }
+                    break;
+                case "Multiplicação":
+                    resultado = num1 * num2;
+                    break;
+                case "Subtração":
+                    resultado = num1 - num2;
+                    break;
+                default:
+                    break;
+            }
+
+        return page.data("result", resultado);
+    }
+
+    
+
 
 }
