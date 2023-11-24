907adf351f7a79cd2c22435663e4849c252541e2
rubemoon <aprovadosatisfy@gmail.com>
2023-11-24 11:33:43 -0300

    updated the celcoin token

--- Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/dto/response/CelcoinTokenDTO.java
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/dto/response/CelcoinTokenDTO.java
@@ -1,40 +1,24 @@
 package com.fag.infra.celcoin.dto;
 
 import jakarta.json.bind.annotation.JsonbProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CelcoinTokenDTO {
-    
-    @JsonbProperty("access_token")
-    private String accessToken;
 
-    @JsonbProperty("expires_in")
-    private Integer expiresIn;
+  @JsonbProperty("access_token")
+  private String accessToken;
 
-    @JsonbProperty("token_type")
-    private String tokenType;
+  @JsonbProperty("expires_in")
+  private Integer expiresIn;
 
-    public String getAccessToken() {
-        return accessToken;
-    }
+  @JsonbProperty("token_type")
+  private String tokenType;
 
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public Integer getExpiresIn() {
-        return expiresIn;
-    }
-
-    public void setExpiresIn(Integer expiresIn) {
-        this.expiresIn = expiresIn;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
+  public String getAccessToken() {
+    return accessToken;
+  }
 
 }
