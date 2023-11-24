41f4effc28863c19678bedfb452a3cb370f66d5a
rubemoon <aprovadosatisfy@gmail.com>
2023-11-24 11:24:05 -0300

    refactoring

---
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/dto/response/CelcoinRechargeValueDTO.java
@@ -0,0 +1,13 @@
+package main.java.com.fag.infra.celcoin.dto.request;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CelcoinRechargeValueDTO {
+
+  private Double value;
+
+}
