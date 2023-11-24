10464d562b5eb5b3f1fe62cbf2bff0300db3f85d
rubemoon <aprovadosatisfy@gmail.com>
2023-11-24 10:44:58 -0300

    refactoring Dtos add lambok library

--- Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/domain/dto/OperatorDTO.java
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/domain/dto/OperatorDTO.java
@@ -1,55 +1,17 @@
 package com.fag.domain.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class OperatorDTO {
 
-    private Integer category;
-
-    private String name;
-
-    private Integer operatorId;
-
-    private Double maxValue;
-
-    private Double minValue;
-
-    public Integer getCategory() {
-        return category;
-    }
-
-    public void setCategory(Integer category) {
-        this.category = category;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getOperatorId() {
-        return operatorId;
-    }
-
-    public void setOperatorId(Integer providerId) {
-        this.operatorId = providerId;
-    }
-
-    public Double getMaxValue() {
-        return maxValue;
-    }
-
-    public void setMaxValue(Double maxValue) {
-        this.maxValue = maxValue;
-    }
-
-    public Double getMinValue() {
-        return minValue;
-    }
-
-    public void setMinValue(Double minValue) {
-        this.minValue = minValue;
-    }
-
+  private Integer category;
+  private Integer rechargeType;
+  private String name;
+  private Integer providerId;
+  private Double maxValue;
+  private Double minValue;
+  
 }
--- Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/domain/dto/ProductDTO.java
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/domain/dto/ProductDTO.java
@@ -1,35 +1,16 @@
 package com.fag.domain.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductDTO {
 
-    private Integer due;
-
-    private String name;
-
-    private Double value;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String productName) {
-        this.name = productName;
-    }
-
-    public Double getValue() {
-        return value;
-    }
-
-    public void setValue(Double value) {
-        this.value = value;
-    }
-
-    public Integer getDue() {
-        return due;
-    }
-
-    public void setDue(Integer dueProduct) {
-        this.due = dueProduct;
-    }
+  private Integer code;
+  private Integer cost;
+  private Integer dueProduct;
+  private String name;
+  private Double value;
 
 }
