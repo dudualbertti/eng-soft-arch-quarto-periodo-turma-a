7ebbc0b5d35e1faf4e4d68a09eb4f67faffc1caa
rubemoon <aprovadosatisfy@gmail.com>
2023-11-24 10:56:55 -0300

    updated the phone mapper

--- Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/domain/mappers/PhoneMapper.java
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/domain/mappers/PhoneMapper.java
@@ -1,22 +1,22 @@
 package com.fag.domain.mappers;
 
-import com.fag.domain.dto.PhoneDTO;
-import com.fag.domain.entities.PhoneBO;
+import br.com.fag.domain.dto.PhoneDTO;
+import br.com.fag.domain.entities.PhoneBO;
 
 public class PhoneMapper {
 
-    public static PhoneBO toBO(PhoneDTO dto) {
-        return new PhoneBO(dto.getStateCode(), dto.getCountryCode(), dto.getNumber());
-    }
+  public static PhoneBO toBO(PhoneDTO dto) {
+    return new PhoneBO(dto.getStateCode(), dto.getCountryCode(), dto.getNumber());
+  }
 
-    public static PhoneDTO toDTO(PhoneBO bo) {
-        PhoneDTO phone = new PhoneDTO();
+  public static PhoneDTO toDTO(PhoneBO bo) {
+    PhoneDTO phone = new PhoneDTO();
 
-        phone.setCountryCode(bo.getCountryCode());
-        phone.setStateCode(bo.getStateCode());
-        phone.setNumber(bo.getNumber());
+    phone.setCountryCode(bo.getCountryCode());
+    phone.setStateCode(bo.getStateCode());
+    phone.setNumber(bo.getNumber());
 
-        return phone;
-    }
+    return phone;
+  }
 
 }
