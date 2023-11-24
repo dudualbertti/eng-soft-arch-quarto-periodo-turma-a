ad65ed2076ecdefcae033569dbc2e4ca22e502a7
rubemoon <aprovadosatisfy@gmail.com>
2023-11-24 11:45:28 -0300

    updated the phone  mapper

--- Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/mappers/CelcoinRechargePhoneMapper.java
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/mappers/CelcoinRechargePhoneMapper.java
@@ -5,24 +5,24 @@ import com.fag.infra.celcoin.dto.CelcoinPhoneDTO;
 
 public class CelcoinRechargePhoneMapper {
 
-    public static PhoneDTO toAppDTO(CelcoinPhoneDTO vendorDTO) {
-        PhoneDTO appDTO = new PhoneDTO();
+  public static PhoneDTO toAppDTO(CelcoinPhoneDTO vendorDTO) {
+    PhoneDTO appDTO = new PhoneDTO();
 
-        appDTO.setStateCode(vendorDTO.getStateCode());
-        appDTO.setCountryCode(vendorDTO.getCountryCode());
-        appDTO.setNumber(vendorDTO.getNumber());
+    appDTO.setStateCode(vendorDTO.getStateCode());
+    appDTO.setCountryCode(vendorDTO.getCountryCode());
+    appDTO.setNumber(vendorDTO.getNumber());
 
-        return appDTO;
-    }
+    return appDTO;
+  }
 
-    public static CelcoinPhoneDTO toVendorDTO(PhoneDTO appDTO) {
-        CelcoinPhoneDTO vendorDTO = new CelcoinPhoneDTO();
+  public static CelcoinPhoneDTO toVendorDTO(PhoneDTO appDTO) {
+    CelcoinPhoneDTO vendorDTO = new CelcoinPhoneDTO();
 
-        vendorDTO.setStateCode(appDTO.getStateCode());
-        vendorDTO.setCountryCode(appDTO.getCountryCode());
-        vendorDTO.setNumber(appDTO.getNumber());
+    vendorDTO.setStateCode(appDTO.getStateCode());
+    vendorDTO.setCountryCode(appDTO.getCountryCode());
+    vendorDTO.setNumber(appDTO.getNumber());
 
-        return vendorDTO;
-    }
+    return vendorDTO;
+  }
 
 }
