62c85214a13a466bd1f5fd17ec4ba4c442761af6
rubemoon <aprovadosatisfy@gmail.com>
2023-11-24 11:42:52 -0300

    updated the celcoin merchant

--- Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/mappers/CelcoinOperatorMapper.java
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/mappers/CelcoinOperatorMapper.java
@@ -5,16 +5,17 @@ import com.fag.infra.celcoin.dto.CelcoinOperatorDTO;
 
 public class CelcoinOperatorMapper {
 
-    public static OperatorDTO toAppDTO(CelcoinOperatorDTO vendorDTO) {
-        OperatorDTO appDTO = new OperatorDTO();
+  public static OperatorDTO toAppDTO(CelcoinOperatorDTO vendorDTO) {
+    OperatorDTO appDTO = new OperatorDTO();
 
-        appDTO.setCategory(vendorDTO.getCategory());
-        appDTO.setMaxValue(vendorDTO.getMaxValue());
-        appDTO.setMinValue(vendorDTO.getMinValue());
-        appDTO.setName(vendorDTO.getName());
-        appDTO.setOperatorId(vendorDTO.getProviderId());
+    appDTO.setCategory(vendorDTO.getCategory());
+    appDTO.setMaxValue(vendorDTO.getMaxValue());
+    appDTO.setMinValue(vendorDTO.getMinValue());
+    appDTO.setName(vendorDTO.getName());
+    appDTO.setProviderId(vendorDTO.getProviderId());
+    appDTO.setRechargeType(vendorDTO.getRechargeType());
 
-        return appDTO;
-    }
+    return appDTO;
+  }
 
 }
