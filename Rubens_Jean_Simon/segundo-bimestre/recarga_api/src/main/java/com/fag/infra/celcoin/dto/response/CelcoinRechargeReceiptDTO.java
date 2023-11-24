4659c3cf4ed5592ce8ccab69dd5530f02f84adb0
rubemoon <aprovadosatisfy@gmail.com>
2023-11-24 11:29:32 -0300

    updated the celcoin receipt

--- Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/dto/response/CelcoinRechargeReceiptDTO.java
+++ Rubens_Jean_Simon/segundo-bimestre/recarga_api/src/main/java/com/fag/infra/celcoin/dto/response/CelcoinRechargeReceiptDTO.java
@@ -1,25 +1,13 @@
 package com.fag.infra.celcoin.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CelcoinRechargeReceiptDTO {
 
-    private String receiptData;
+  private String receiptData;
+  private String receiptformatted;
 
-    private String receiptformatted;
-
-    public String getReceiptData() {
-        return receiptData;
-    }
-
-    public void setReceiptData(String receiptData) {
-        this.receiptData = receiptData;
-    }
-
-    public String getReceiptformatted() {
-        return receiptformatted;
-    }
-
-    public void setReceiptformatted(String receiptformatted) {
-        this.receiptformatted = receiptformatted;
-    }
-    
 }
